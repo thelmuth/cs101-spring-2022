f025fb4977fd664302e147655fca10aa46d19f98
Tom Helmuth <thelmuth@hamilton.edu>
2022-01-21 11:36:35 -0500

    Code from 1/21/22

---
+++ Class02/first_turtle_program.py
@@ -0,0 +1,28 @@
+
+## Need these things in order to use turtles:
+
+from turtle import *
+
+colormode(255) # This makes it easier to manipulate colors
+
+# This is our turtle, stored in a variable named leo
+leo = Turtle()
+
+leo.speed(1) # slows down leo so we can see what's happening
+leo.width(3) # Increases the pen's width to 3 pixels wide
+
+# This tells leo to move forward 80 pixels
+leo.forward(80)
+leo.left(60)
+leo.forward(110)
+
+leo.right(150)
+leo.forward(200)
+leo.left(60)
+leo.backward(300)
+
+
+# shirley = Turtle()
+# 
+# shirley.right(90)
+# shirley.forward(200)
---
+++ Class02/small_program.py
@@ -0,0 +1,12 @@
+
+## A program is a file on a computer with instructions telling the computer what to do.
+## This is a comment -- is ignored by Python
+
+cat = 5 + 2
+print(cat)
+
+## Variable names can be any letters, numbers, or underscores - start with a letter
+
+dog_version_2 = cat * 9
+print(dog_version_2)
+
